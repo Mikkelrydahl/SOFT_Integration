910c8e7fc213303260ca39f631bcc16080e84162
Mikkelrydahl <Mikkelrydahloest@gmail.com>
2024-02-08 00:27:24 +0100

    Data parser
    Lavede et python script som kan læse og udskrive mine datafiler.

---
+++ 02_Data_Files/data_parser.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import yaml
+import xml.etree.ElementTree as ET
+
+#read and parse CSV
+with open('me.csv', 'r') as csv_file:
+    csv_reader = csv.dictReader(csv_file)
+    csv_data = [row for row in csv_reader]
+
+# Read and parse JSON file
+with open('me.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+# Read and parse YAML file
+with open('me.yaml', 'r') as yaml_file:
+    yaml_data = yaml.safe_load(yaml_file)
+
+# Read and parse XML file
+xml_tree = ET.parse('me.xml')
+xml_root = xml_tree.getroot()
+xml_data = []
+for people in xml_root.findall('people'):
+    people_data = {
+        'name': people.find('name').text,
+        'age': int(people.find('age').text),
+        'hobbies': people.find('hobbies').text
+    }
+    xml_data.append(people_data)
+
+print("CSV Data:", csv_data)
+print("JSON Data:", json_data)
+print("YAML Data:", yaml_data)
+print("XML Data:", xml_data)
